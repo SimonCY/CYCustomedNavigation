9b3b42b185fc9ed1f2ea27df1e5ee621daba572b
陈炎 <chenyan@deepai.cn>
2017-11-20 15:56:40 +0800

    init

---
+++ CYNavigaitonControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYNavigaitonControllerDemo
+//
+//  Created by DeepAI on 2017/11/20.
+//  Copyright © 2017年 DeepAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
